3980af2e16bcd820c1bccc88bb3b222da7ee2f35
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 10:27:26 -0400

    Update Team.java

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,35 +1,72 @@
 import java.util.ArrayList;
-
 public class Team {
-    private String teamColor;
-    private ArrayList<Unit> teamUnits;
-    public Team(String teamColor, ArrayList<Unit> teamUnits){
+    /** This class creates a Team for the game with a specific color and specific
+     * units that belong to that team
+     * @author TaraLennon & Khadija Mohammadi
+     * @verison 1
+     */
+    String teamColor;
+    //represents the team color
+    ArrayList<Unit> teamUnits;
+    //arraylist of Units that belong to the team
+
+    public Team (String teamColor, ArrayList<Unit> teamUnits){
+        //two parameter constructor
         this.teamColor = teamColor;
         this.teamUnits = teamUnits;
-
+        for (int i = 0; i < teamUnits.size(); i++){
+            teamUnits.get(i).setTeamColor(teamColor);
+        }
     }
-    public String getTeamColor(){
+    /**
+     * This method gets the team color
+     * @return String that represents the team color
+     */
+    public String getTeamColor() {
         return teamColor;
     }
-
+    /**
+     * This method gets the array list of team units
+     * @return an array list of the team units
+     */
     public ArrayList<Unit> getTeamUnits() {
         return teamUnits;
     }
-    public void removeUnitsFromTeam(Unit teamUnitBeRemoved){
-        teamUnits.remove(teamUnitBeRemoved);
-    }
-    public void addUnitsToTeam(Unit teamUnitBeAdded){
-        teamUnits.add(teamUnitBeAdded);
-    }
-
-    @Override
-    public String toString() {
-        String s = "";
-        for(Unit elm: teamUnits){
-            s += elm.toString();
+    /**
+     * This method removes specific units from the team
+     * @param unitRemoved a Unit that is intended to be removed from the array list
+     */
+    public void removeUnitsFromTeam (Unit unitRemoved){
+        for (int i = 0; i < teamUnits.size(); i++){
+            if (teamUnits.get(i) == unitRemoved){
+                teamUnits.remove(i);
+            }
         }
 
-        return "Team " + teamColor + " Units :\n" + s;
     }
-}
 
+    /**
+     * This method adds unit to the team
+     * @param addUnit a Unit that is intended to be added to the array list
+     */
+
+    public void addUnitsToTeam (Unit addUnit){
+        addUnit.setTeamColor(teamColor);
+        teamUnits.add(addUnit);
+
+    }
+
+    /**
+     * This method overrides toString
+     * @return an String that represents the team
+     */
+    @Override
+    public String toString (){
+        String origString = "";
+        for (int i = 0; i < teamUnits.size(); i ++){
+            origString = origString + teamUnits.get(i).toString() + " ";
+        }
+        return "Team " + teamColor + " Units :" + "\n" + origString;
+    }
+
+}
