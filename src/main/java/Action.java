a09b2cdacb2282b57905cfa24c2745f0ea8f8d14
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 13:29:17 -0400

    Update Action.java

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,21 +1,29 @@
-/**  This class has an abstract method called "performAction()" which does not have
- * an implementation in this class. Any class that extends this abstract class
- * will need to implement the "performAction()" method.
+/**  This represents actions that the players
+ * can make during their turn.
  * @author TaraLennon & Khadija Mohammadi
  * @verison 1
  */
 public abstract class Action {
-     Game theGame;
-     int rowNumsFrom;
-     int columnNumsFrom;
-     int rowNumsTo;
-     int columnsNumsTo;
-     Action(Game theGame, int rowNumsFrom, int columnNumsFrom, int rowNumsTo, int columnsNumsTo){
-          this.theGame = theGame;
-          this.rowNumsFrom = rowNumsFrom;
-          this.columnNumsFrom = columnNumsFrom;
-          this. rowNumsTo = rowNumsTo;
-          this.columnsNumsTo = columnsNumsTo;
-     }
-     public abstract void performAction();
+    Game theGame;
+    //represents the game being played
+    int rowNumsFrom;
+    //represents row index of from piece
+    int columnNumsFrom;
+    //column index of from piece
+    int rowNumsTo;
+    //row index of to piece
+    int columnsNumsTo;
+    //column index of to piece
+
+    Action(Game theGame, int rowNumsFrom, int columnNumsFrom, int rowNumsTo, int columnsNumsTo){
+        //5 parameter constructor
+        this.theGame = theGame;
+        this.rowNumsFrom = rowNumsFrom;
+        this.columnNumsFrom = columnNumsFrom;
+        this.rowNumsTo = rowNumsTo;
+        this.columnsNumsTo = columnsNumsTo;
+    }
+    public abstract void performAction();
+    //abstract method to be implemented in subclasses
 }
+
