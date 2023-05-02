6e407bfb67e2c9bc2acb8ceef4ef799a251f9af6
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 10:38:09 -0400

    Update PlayerTest.java

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,35 +1,80 @@
+
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-
 public class PlayerTest {
+
+    /** This class tests the Player Class
+     * @author TaraLennon & Khadija Mohammadi
+     * @verison 1
+     */
+
+    /** This method tests the player class with a no parameter constructor
+     *
+     */
+
     @Test
-    public void testSetPlayerNumber() {
-        Player playerNew = new Player();
-        assertEquals(3, playerNew.getPlayerNumber());
-        playerNew.setPlayerNumber(1);
-        assertEquals(1, playerNew.getPlayerNumber());
+    public void testingPlayerNoParameter() {
+        System.out.println("Testing No Parameter Constructor");
+        Player newPlayer = new Player();
+        System.out.println("-- Testing Player Number");
+        assertEquals(1, newPlayer.getPlayerNumber());
+        newPlayer.setPlayerNumber(2);
+        assertEquals(2, newPlayer.getPlayerNumber());
+        System.out.println("-- Testing Turn");
+        assertFalse(newPlayer.isTurn());
+        newPlayer.setTurn(true);
+        assertTrue(newPlayer.isTurn());
+        System.out.println("-- Testing Team");
+        ArrayList<Unit> teamUnits = new ArrayList<Unit>();
+        Team newTeam = new Team("blue", teamUnits);
+        newPlayer.setTeam(newTeam);
+        assertSame(newTeam, newPlayer.getTeam());
 
     }
+
+    /** This method tests the player class with a two and three parameter constructor
+     *
+     */
+
     @Test
-    public void testTurn() {
-        Player playerNew = new Player();
-        assertEquals(false, playerNew.isTurn());
-        playerNew.setTurn(true);
-        assertEquals(true, playerNew.isTurn());
-    }
-    @Test
-    public void testTeam() {
-        ArrayList<Unit> element= new ArrayList<>();
-        element.add(new TomJerryUnit());
-        Team teamA = new Team("pink",  element);
-        Player playerNew = new Player(1, teamA);
-        assertEquals(teamA, playerNew.getTeam());
-        Team teamB = new Team("black", element);
-        playerNew.setTeam(teamB);
-        assertEquals(teamB, playerNew.getTeam());
+    public void testingPlayerParameterConstructor() {
+        System.out.println("Testing two parameter constructor");
+        ArrayList<Unit> teamUnit = new ArrayList<Unit>();
+        Team newTeam3 = new Team("green", teamUnit);
+        Player newPlayer5 = new Player(123, newTeam3);
+        System.out.println("-- Testing Player Number");
+        assertEquals(123, newPlayer5.getPlayerNumber());
+        newPlayer5.setPlayerNumber(5);
+        assertEquals(5, newPlayer5.getPlayerNumber());
+        System.out.println("-- Testing Turn");
+        assertFalse(newPlayer5.isTurn());
+        newPlayer5.setTurn(true);
+        assertTrue(newPlayer5.isTurn());
+        System.out.println("-- Testing Team");
+        assertSame(newTeam3, newPlayer5.getTeam());
+        Team newTeam6 = new Team("orange", teamUnit);
+        newPlayer5.setTeam(newTeam6);
+        assertSame(newTeam6, newPlayer5.getTeam());
+        System.out.println("Testing Three Parameter Constructor");
+        ArrayList<Unit> teamU = new ArrayList<Unit>();
+        Team newTeam1 = new Team("green", teamU);
+        Player newPlayer = new Player(123, true, newTeam1);
+        System.out.println("-- Testing Player Number");
+        assertEquals(123, newPlayer.getPlayerNumber());
+        newPlayer.setPlayerNumber(5);
+        assertEquals(5, newPlayer.getPlayerNumber());
+        System.out.println("-- Testing Turn");
+        assertTrue(newPlayer.isTurn());
+        newPlayer.setTurn(false);
+        assertFalse(newPlayer.isTurn());
+        System.out.println("-- Testing Team");
+        assertSame(newTeam1, newPlayer.getTeam());
+        Team newTeam2 = new Team("red", teamU);
+        newPlayer.setTeam(newTeam2);
+        assertSame(newTeam2, newPlayer.getTeam());
+
     }
 }
