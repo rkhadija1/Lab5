4d6eb0ebf8a9704b064aded3614aef1f965a58d6
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 09:35:24 -0400

    Update Action.java

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,10 +1,22 @@
+/**  This represents actions that the players 
+ * can make during their turn. 
+ * @author TaraLennon & Khadija Mohammadi
+ * @verison 1
+ */
 public abstract class Action {
      Game theGame;
+     //represents the game being played
      int rowNumsFrom;
+     //represents row index of from piece
      int columnNumsFrom;
+     //column index of from piece
      int rowNumsTo;
+     //row index of to piece
      int columnsNumsTo;
+     //column index of to piece
+     
      Action(Game theGame, int rowNumsFrom, int columnNumsFrom, int rowNumsTo, int columnsNumsTo){
+          //5 parameter constructor
           this.theGame = theGame;
           this.rowNumsFrom = rowNumsFrom;
           this.columnNumsFrom = columnNumsFrom;
@@ -12,4 +24,5 @@ public abstract class Action {
           this.columnsNumsTo = columnsNumsTo;
      }
      public abstract void performAction();
+     //abstract method to be implemented in subclasses
 }
