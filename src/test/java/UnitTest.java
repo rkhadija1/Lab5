5c4c3c2fd00cd4f0d60f693f1d336d208eb9120b
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 10:39:17 -0400

    Update UnitTest.java

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,14 +1,22 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-
 public class UnitTest {
+    /** This class tests the methods in the Unit class
+     * @author TaraLennon & Khadija Mohammadi
+     * @verison 1
+     */
 
     @Test
     public void test_Unit() {
+        /**
+         * Tests the methods in the Unit class
+         */
 
         // Unit has a constructor
         // but we should not be able to create a Unit
@@ -49,6 +57,9 @@ public class UnitTest {
 
     @Test
     public void test_TomJerryUnit() {
+        /**
+         * Tests the method tests if tom and jerry unit successfully extends the unit class
+         */
 
         System.out.println("Testing Tom & Jerry Unit Class");
         Unit tj = new TomJerryUnit();
@@ -75,6 +86,9 @@ public class UnitTest {
 
     @Test
     public void test_BartSimpsonUnit() {
+        /**
+         * Tests the method tests if bart simpson unit successfully extends the unit class
+         */
 
         System.out.println("Testing Bart Simpson Unit Class");
         BartSimpsonUnit bart = new BartSimpsonUnit();
