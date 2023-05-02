9a0af3bd65c3eafb7107d01ab315697e1ae35a6d
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 10:20:56 -0400

    Update GameEventsLinkedList.java

--- src/main/java/GameEventsLinkedList.java
+++ src/main/java/GameEventsLinkedList.java
@@ -1,99 +1,145 @@
-/**
- * <h1>Lab6</h1>
- * <h2>CISC181040L Spring 2023</h2>
- * <h3>University of Delaware</h3>
- * <p>
- * The Lab6 is going to be a practice of using limked list, which is going to be very different from Array list.
- * this lab will also include a set of removing, adding, and Traversing practices. As usual a couple
- * of setters and getters and constructors for each class will be practiced.
- * * @author  Khadija Mohammadi
- * @since 2023-04-24
- */
 public class GameEventsLinkedList {
+    /** This class creates a game events linked list
+     * @author TaraLennon
+     * @verison 1
+     */
     private GameEventNode head;
+    //represents the first game event
     private int size;
-    public GameEventsLinkedList(){
-        this.head = null;
-        this.size = 0;
-    }
-    public boolean isEmpty(){
-        return size == 0;
-    }
-    public void add(GameEventNode node){
-        if(head == null){
-            head = node;
-        } else{
-            GameEventNode current = head;
-            while(current.getNext() != null){
-                current = current.getNext();
-            }
-            current.setNext(node);
-        }
-        size++;
-    }
-    public GameEvent remove(GameEventNode node){
-        GameEvent event = null;
-        if(head == null){
-            return null;
-        }
-        if (head == node) {
-            head = head.getNext();
-            event = node.getGameState();
-            node.setNext(null);
-            size--;
-        } else {
-            GameEventNode current = head;
-            while (current.getNext() != null && current.getNext() != node) {
-                current = current.getNext();
-            }
-            if (current.getNext() == node) {
-                current.setNext(node.getNext());
-                event = node.getGameState();
-                node.setNext(null);
-                size--;
-            }
-        }
-        return event;
-    }
-    public GameEventsLinkedList extract(String eventType) {
-        GameEventsLinkedList extractedList = new GameEventsLinkedList();
-        GameEventNode current = head;
-        while (current != null) {
-            if (current.getGameState().getEventType().equals(eventType)) {
-                extractedList.add(new GameEventNode(current.getGameState()));
-            }
-            current = current.getNext();
-        }
-        return extractedList;
-    }
-    public void display() {
-        GameEventNode current = head;
-        while (current != null) {
-            System.out.println(current.getGameState().getEventDetails());
-            current = current.getNext();
-        }
-    }
+    //stores the size of the linked list
 
+    public GameEventsLinkedList() {
+        //0 parameter constructor, creates an empty linked list
+        head = null;
+        size = 0;
+    }
+    /**
+     * Gets the size of the linked list
+     * @return int representing the size of the linked list
+     */
     public int getSize() {
         return size;
     }
-
+    /**
+     * Gets the head of the linked list
+     * @return GameEventNode representing the head of the list
+     */
     public GameEventNode getHead() {
         return head;
     }
-
-    public GameEventNode getNodeAtIndex(int i) {
-        GameEventNode current = head;
-        int counter = 0;
-        while (current != null) {
-            System.out.println(current.getGameState().getEventDetails());
-            if(counter == i){
-                return current;
-            }
-            current = current.getNext();
-
-            counter++;
-        }
-        return null;
+    /**
+     * Returns whether a list is empty or not
+     * @return boolean representing whether a list is empty or not
+     */
+    public boolean isEmpty() {
+        return size == 0;
     }
+    /**
+     * Adds a GameEventNode to a linked list
+     * @param event GameEventNode representing the next event in a linked list
+     */
+    public void add(GameEventNode event) {
+        if (this.head == null) {
+            //adds the event to the top of the list if the list is empty
+            this.head = event;
+            GameEventNode current = this.head;
+            while (!(current == null)) {
+                this.size = this.size + 1;
+                current = current.getNext();
+            }
+        }
+        else {
+            //branches off if list is not empty
+            GameEventNode current = this.head;
+            while (!(current == null)) {
+                if (current.getNext() == null) {
+                    //adds event if the next field of current is null
+                    current.setNext(event);
+                    current = current.getNext();
+                    while (!(current == null)) {
+                        this.size = this.size + 1;
+                        current = current.getNext();
+                    }
+                }
+                else {
+                    //branches off if the next field of current is not null, and gets the next field
+                    current = current.getNext();
+                }
+            }
+
+        }
+
+
+
+
+    }
+    /**
+     * Removes a GameEventNode from a linked list
+     * @param event GameEventNode to be removed
+     * @return GameEvent of the GameEvent node that is being returned, or
+     * if nothing is being removed, then the GameEventNode of the event the method's argument
+     */
+    public GameEvent remove (GameEventNode event){
+        GameEventNode current = this.head;
+        while (!(current == null)){
+            if (current == event){
+                //removes the event if the current GameEventNode matches the method's argument
+                GameEventNode nodeToRemove = current;
+                this.head = current.getNext();
+                size = size - 1;
+                return nodeToRemove.getGameState();
+
+            }
+            else if (current.getNext() == event){
+                //removes the event if the current's next field matches the method's argument
+                GameEventNode nodeToRemove = current.getNext();
+                current.setNext(nodeToRemove.getNext());
+                this.size = this.size - 1;
+                return nodeToRemove.getGameState();
+            }
+            //gets the next field if nothing matched the argument
+            current = current.getNext();
+        }
+        //returns the argument's game state if nothing is being removed
+        return event.getGameState();
+    }
+    /**
+     * Extracts a new list from a linked list of a certain event type
+     * @param _eventType representing the event type to be extracted
+     * @return new GameEventsLinkedList of the extracted nodes
+     */
+    public GameEventsLinkedList extract (String _eventType){
+        GameEventNode current = this.head;
+        //creating empty new list
+        GameEventsLinkedList newList = new GameEventsLinkedList();
+        while (!(current == null)){
+            if (current.getGameState().getEventType().equals(_eventType)){
+                //branches off if current's event type equals the event type to be extracted & adds to new list
+                GameEventNode addNode = new GameEventNode(current.getGameState());
+                newList.add(addNode);
+            }
+            //gets the next node if nothing matched
+            current = current.getNext();
+        }
+        return newList;
+
+        }
+
+
+    /**
+     * navigates the nodes of the list and prints each node's event details
+     */
+    public void display (){
+        GameEventNode current = this.head;
+        while (!(current == null)) {
+            System.out.println(current.getGameState().getEventDetails());
+            current = current.getNext();
+        }
+    }
+
+    public static void main (String args []){
+        System.out.println("main");
+    }
+
+
 }
