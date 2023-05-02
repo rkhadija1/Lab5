a3e8d3638752f8e70b26682e4c14b04dde03f0e1
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-02 10:38:54 -0400

    Update TomJerryUnitTest.java

--- src/test/java/TomJerryUnitTest.java
+++ src/test/java/TomJerryUnitTest.java
@@ -1,12 +1,21 @@
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class TomJerryUnitTest {
+
+    /** This class tests the methods in the TomJerryUnit class.
+     * @author TaraLennon & Khadija Mohammadi
+     * @verison 1
+     */
     @Test
     public void testingTomJerryNoParameterConstructor() {
+        /**
+         * Tests the methods in the TomJerryUnit class with a no parameter constructor
+         */
         System.out.println("Creating Tom & Jerry with 0 parameter constructor");
         TomJerryUnit tj = new TomJerryUnit();
+        System.out.println("Testing if Tom & Jerry is an instance of Unit class");
+        assertTrue(tj instanceof Unit);
         System.out.println();
         System.out.println("-- Testing Getters");
         System.out.println();
@@ -32,10 +41,12 @@ public class TomJerryUnitTest {
         assertEquals(1, tj.getMovement());
         System.out.println("-- Testing Movement Modifier");
         assertEquals(0, tj.getMovementModifier());
+        System.out.println("-- Testing team color");
+        assertEquals("red", tj.getTeamColor());
         System.out.println("-- Testing Homing Rocket");
-        assertTrue(tj.canHomingRocket());
+        assertTrue(tj.isHomingRocket());
         System.out.println("-- Testing Offer Cheese");
-        assertTrue(tj.canOfferCheese());
+        assertTrue(tj.isOfferCheese());
         System.out.println("-- Testing Hiding");
         assertFalse(tj.isHiding());
         System.out.println();
@@ -74,12 +85,15 @@ public class TomJerryUnitTest {
         System.out.println("-- Testing Movement Modifier");
         tj.setMovementModifier(2);
         assertEquals(2, tj.getMovementModifier());
+        System.out.println("-- Testing team color");
+        tj.setTeamColor("orange");
+        assertEquals("orange", tj.getTeamColor());
         System.out.println("-- Testing Homing Rocket");
         tj.setHomingRocket(false);
-        assertFalse(tj.canHomingRocket());
+        assertFalse(tj.isHomingRocket());
         System.out.println("-- Testing Offer Cheese");
         tj.setOfferCheese(false);
-        assertFalse(tj.canOfferCheese());
+        assertFalse(tj.isOfferCheese());
         System.out.println("-- Testing Hiding");
         tj.setHiding(true);
         assertTrue(tj.isHiding());
@@ -96,40 +110,54 @@ public class TomJerryUnitTest {
         tj.setHiding(false);
         tj.takeDamage(25.0);
         assertEquals(65.0, tj.getHealth(), 0.0001);
+        System.out.println("-- Testing Spawn");
+        TomJerryUnit spawntj = new TomJerryUnit('t', "Tom & Jerry", 100.0, 5.0, 25.0, 10.0, 0, 1, 1, 1, 1, "red", true, true, false);
+        assertEquals(spawntj.toString(), tj.spawn().toString());
+        assertEquals("red t", tj.spawn().toString());
+
     }
     @Test
     public void testingTomJerryParameterConstructor() {
-        System.out.println("Creating Tom & Jerry with 14 parameter constructor");
-        TomJerryUnit tj = new TomJerryUnit();
+        /**
+         * Tests the methods in the TomJerryUnit class with a 15 parameter constructor
+         */
+        System.out.println("Creating Tom & Jerry with 15 parameter constructor");
+        TomJerryUnit tj = new TomJerryUnit('T', "Tom And Jerry", 100.0, 10.0,
+                25.0, 5.0, 1, 1, 1, 0, 1,
+                "pink", true, true, false);
         System.out.println();
+        System.out.println("Testing if Tom & Jerry is an instance of Unit class");
+        assertTrue(tj instanceof Unit);
         System.out.println("-- Testing Getters");
         System.out.println();
         System.out.println("-- Testing Symbol");
         assertEquals('T', tj.getSymbol());
         System.out.println("-- Testing Name");
-        assertEquals("Tom & Jerry", tj.getName());
+        assertEquals("Tom And Jerry", tj.getName());
         System.out.println("-- Testing Health");
         assertEquals(100.0, tj.getHealth(), 0.0001);
         System.out.println("-- Testing Health Modifier");
-        assertEquals(0.0, tj.getHealthModifier(), 0.0001);
+        assertEquals(10.0, tj.getHealthModifier(), 0.0001);
         System.out.println("-- Testing Damage");
         assertEquals(25.0, tj.getDamage(), 0.0001);
         System.out.println("-- Testing Damage Modifier");
-        assertEquals(0.0, tj.getDamageModifier(), 0.0001);
+        assertEquals(5.0, tj.getDamageModifier(), 0.0001);
         System.out.println("-- Testing Luck");
-        assertEquals(0, tj.getLuck());
+        assertEquals(1, tj.getLuck());
         System.out.println("-- Testing xCor");
-        assertEquals(5, tj.getxCor());
+        assertEquals(1, tj.getxCor());
         System.out.println("-- Testing yCor");
-        assertEquals(5, tj.getyCor());
+        assertEquals(1, tj.getyCor());
         System.out.println("-- Testing Movement");
-        assertEquals(1, tj.getMovement());
+        assertEquals(0, tj.getMovement());
         System.out.println("-- Testing Movement Modifier");
-        assertEquals(0, tj.getMovementModifier());
+        assertEquals(1, tj.getMovementModifier());
+        System.out.println("-- Testing team color");
+        assertEquals("pink", tj.getTeamColor());
         System.out.println("-- Testing Homing Rocket");
-        assertTrue(tj.canHomingRocket());
+        assertTrue(tj.isHomingRocket());
         System.out.println("-- Testing Offer Cheese");
-        assertTrue(tj.canOfferCheese());
+        assertTrue(tj.isOfferCheese());
         System.out.println("-- Testing Hiding");
         assertFalse(tj.isHiding());
         System.out.println();
@@ -168,12 +196,15 @@ public class TomJerryUnitTest {
         System.out.println("-- Testing Movement Modifier");
         tj.setMovementModifier(2);
         assertEquals(2, tj.getMovementModifier());
+        System.out.println("-- Testing team color");
+        tj.setTeamColor("black");
+        assertEquals("black", tj.getTeamColor());
         System.out.println("-- Testing Homing Rocket");
         tj.setHomingRocket(false);
-        assertFalse(tj.canHomingRocket());
+        assertFalse(tj.isHomingRocket());
         System.out.println("-- Testing Offer Cheese");
         tj.setOfferCheese(false);
-        assertFalse(tj.canOfferCheese());
+        assertFalse(tj.isOfferCheese());
         System.out.println("-- Testing Hiding");
         tj.setHiding(true);
         assertTrue(tj.isHiding());
@@ -190,5 +221,9 @@ public class TomJerryUnitTest {
         tj.setHiding(false);
         tj.takeDamage(25.0);
         assertEquals(65.0, tj.getHealth(), 0.0001);
+        System.out.println("-- Testing Spawn");
+        TomJerryUnit spawntj = new TomJerryUnit('t', "Tom & Jerry", 100.0, 5.0, 25.0, 10.0, 0, 1, 1, 1, 1, "red", true, true, false);
+        assertEquals(spawntj.toString(), tj.spawn().toString());
+        assertEquals("red t", tj.spawn().toString());
     }
 }
