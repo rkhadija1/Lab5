85581beb2dfd0c8e82b0b018deb4926a5c3051e1
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-15 09:54:22 -0400

    Update Oda.java

--- src/main/java/Oda.java
+++ src/main/java/Oda.java
@@ -3,13 +3,17 @@
  * @verison 1
  */
 public class Oda extends TomJerryUnit{
-    public Oda(){
-        super(true, true, false);
-        this.name = "Oda";
-        this.symbol = 'O';
+    public Oda(char symbol, String name, double health, double healthModifier, double damage, double damageModifier, int luck, int xCor, int yCor, int movement, int movementModifier, String teamColor, int numAttacks, boolean homingRocket, boolean offerCheese, boolean hiding)){
+        super(char symbol, String name, double health, double healthModifier, double damage, double damageModifier, int luck, int xCor, int yCor, int movement, int movementModifier, String teamColor, int numAttacks, boolean homingRocket, boolean offerCheese, boolean hiding);
+    }
+
+    public Oda (){
+        this("O", "Oda", 100.0, 0.0, 25.0, 0.0, 0, 8, 8, 1, 0, "red", 0, true, true, false)
     }
     // the different pattern of the validMovePath method is to check if the
     // absolute difference between row and column numbers is even.
+    //FIXME
+    //this method needs a javadoc
     @Override
     public boolean validMovePath(int rowNumsFrom, int columnNumsFrom, int rowNumsTo, int columnsNumsTo){
         // implement a new pattern for valid move paths
@@ -21,5 +25,7 @@ public class Oda extends TomJerryUnit{
             return false;
         }
     }
-}
 
+    //FIXME
+    //you still need to implement the spawn method, the canSpawn method, the validAttackPath method, and the validSpawnPath method
+}
