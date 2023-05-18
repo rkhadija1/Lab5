ca2367b1dfc077564d97a93b59729d2afa0a57ba
taralenns <131809922+taralenns@users.noreply.github.com>
2023-05-18 12:48:22 -0400

    Update Akaza.java

--- src/main/java/Akaza.java
+++ src/main/java/Akaza.java
@@ -1,22 +1,25 @@
-/** This class defines the new game unit which extends Attacker and the validPath methods are different from the ones in
- * TomJerry and BartSimpson
- * @author TaraLennon & Khadija Mohammadi
- * @verison 1
- */
-
+// the new game unit which extends Attacker
+//FIXME
+//we need a javadoc describing the class
 public class Akaza extends Attacker{
-    Akaza(char symbol, String name, double health, double healthModifier, double damage, double damageModifier,
-          int luck, int xCor, int yCor, int movement, int movementModifier, String teamColor) {
+    public Akaza(char symbol, String name, double health, double healthModifier, double damage, double damageModifier,
+          int luck, int xCor, int yCor, int movement, int movementModifier, String teamColor, int numAttacks) {
         super(symbol, name, health, healthModifier, damage, damageModifier, luck,
-                xCor, yCor, movement, movementModifier, teamColor);
+                xCor, yCor, movement, movementModifier, teamColor, numAttacks);
     }
+
+    public Akaza (){
+        this("A", "Akaza", 100.0, 0.0, 25.0, 0.0, 0, 10, 10, 1, 0, "red", 0)
+    }
+
+
     /**
      * Overrides the spawn method from the Unit class.
      * @return a new instance of the Akaza class with the given parameters */
     @Override
     public Unit spawn() {
         return new Akaza(symbol, name, health, healthModifier, damage, damageModifier, luck,
-                    xCor, yCor, movement, movementModifier, teamColor);
+                xCor, yCor, movement, movementModifier, teamColor);
     }
     /**
      * Overrides the canSpawn method from the Unit class.
@@ -27,13 +30,13 @@ public class Akaza extends Attacker{
         return true;
     }
     /**
-    * Overrides the validMovePath method from the Unit class.
-    * A move is valid if the difference in rows and columns is even.
-    * @param rowNumsFrom the starting row number
-    * @param columnNumsFrom the starting column number
-    * @param rowNumsTo the ending row number
-    * @param columnsNumsTo the ending column number
-    * @return true if the move is valid, false otherwise
+     * Overrides the validMovePath method from the Unit class.
+     * A move is valid if the difference in rows and columns is even.
+     * @param rowNumsFrom the starting row number
+     * @param columnNumsFrom the starting column number
+     * @param rowNumsTo the ending row number
+     * @param columnsNumsTo the ending column number
+     * @return true if the move is valid, false otherwise
      */
     @Override
     public boolean validMovePath(int rowNumsFrom, int columnNumsFrom, int rowNumsTo, int columnsNumsTo) {
@@ -46,13 +49,13 @@ public class Akaza extends Attacker{
             return false;}
     }
     /**
-    * Overrides the validSpawnPath method from the Unit class.
-    * A spawn path is valid if the move from (rowNumsFrom, columnNumsFrom) to (rowNumsTo, columnsNumsTo) is valid.
+     * Overrides the validSpawnPath method from the Unit class.
+     * A spawn path is valid if the move from (rowNumsFrom, columnNumsFrom) to (rowNumsTo, columnsNumsTo) is valid.
      * @return true if the move is valid, false otherwise
      */
     @Override
     public boolean validSpawnPath(int rowNumsFrom, int columnNumsFrom, int rowNumsTo, int columnsNumsTo) {
-            return validMovePath(rowNumsFrom, columnNumsFrom, rowNumsTo, columnsNumsTo);
+        return validMovePath(rowNumsFrom, columnNumsFrom, rowNumsTo, columnsNumsTo);
     }
     /**
      Overrides the validAttackPath method from the Unit class.
